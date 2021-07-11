815dd7dd11aa7b7af704b108cba6f74a1ba0f260
ww24kobe <1259481020@qq.com>
2021-07-11 11:27:33 +0800

    feat:dotenv

---
+++ example/dotenvdemo/index.js
@@ -0,0 +1,9 @@
+// const db = require('db')
+// db.connect({
+//     host: process.env.DB_HOST,
+//     username: process.env.DB_USER,
+//     password: process.env.DB_PASS
+// })
+
+require('dotenv').config()
+console.log(process.env.DB_PASS)
