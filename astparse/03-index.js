bef0f382533b50c272231e0c09df6190bc159eba
ww24kobe <1259481020@qq.com>
2021-08-23 22:55:18 +0800

    feat: webapi-for-fullscreen

--- astparse/03-index.js
+++ astparse/03-index.js
@@ -1,14 +1,14 @@
 var MagicString = require('magic-string');
 var s = new MagicString('problems = 99');
 
-console.log( s.overwrite(0, 8, 'answer') );
-console.log( s.toString() ); // 'answer = 99' 
+console.log(s.overwrite(0, 8, 'answer'));
+console.log(s.toString()); // 'answer = 99' 
 
- s.overwrite(11, 13, '42'); // character indices always refer to the original string
-console.log( s.toString() ); // 'answer = 42'
+s.overwrite(11, 13, '42'); // character indices always refer to the original string
+console.log(s.toString()); // 'answer = 42'
 
- s.prepend('var ').append(';'); // most methods are chainable
- console.log( s.toString() ); // 'var answer = 42;'
+s.prepend('var ').append(';'); // most methods are chainable
+console.log(s.toString()); // 'var answer = 42;'
 
 var map = s.generateMap({
     source: 'source.js',
